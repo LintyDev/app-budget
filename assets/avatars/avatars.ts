c16c7876d05bdba6f8b57926f580c0133b74333e
Linty <lintydev@gmail.com>
2024-03-31 01:51:23 +0100

    add assets

---
+++ assets/avatars/avatars.ts
@@ -0,0 +1,45 @@
+export interface Avatars{
+  avatarId: number;
+  avatarPath: any;
+}
+
+const avatars: Avatars[] = [
+  {
+    avatarId: 1,
+    avatarPath: require("assets/avatars/avatar-1.png"),
+  },
+  {
+    avatarId: 2,
+    avatarPath: require("assets/avatars/avatar-2.png"),
+  },
+  {
+    avatarId: 3,
+    avatarPath: require("assets/avatars/avatar-3.png"),
+  },
+  {
+    avatarId: 4,
+    avatarPath: require("assets/avatars/avatar-4.png"),
+  },
+  {
+    avatarId: 5,
+    avatarPath: require("assets/avatars/avatar-5.png"),
+  },
+  {
+    avatarId: 6,
+    avatarPath: require("assets/avatars/avatar-6.png"),
+  },
+  {
+    avatarId: 7,
+    avatarPath: require("assets/avatars/avatar-7.png"),
+  },
+  {
+    avatarId: 8,
+    avatarPath: require("assets/avatars/avatar-8.png"),
+  },
+  {
+    avatarId: 9,
+    avatarPath: require("assets/avatars/avatar-9.png"),
+  },
+];
+
+export default avatars;
