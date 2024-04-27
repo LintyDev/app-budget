8cad3493ccd923caef729f3f64762ee8d82da508
Linty <lintydev@gmail.com>
2024-04-27 22:00:54 +0200

    add HeaderCategories

---
+++ src/components/categories/HeaderCategories.tsx
@@ -0,0 +1,12 @@
+import globalStyles from "@/styles/globalStyles";
+import { Text, View } from "react-native";
+
+function HeaderCategories() {
+  return (
+    <View>
+      <Text style={globalStyles.text}>Je suis le composant infos</Text>
+    </View>
+  )
+}
+
+export default HeaderCategories;
