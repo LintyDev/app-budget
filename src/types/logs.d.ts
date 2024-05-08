6bc4304c2a1afdedd4e0761ea353b255d3726531
Linty <lintydev@gmail.com>
2024-05-08 15:28:08 +0200

    add type to log

--- src/types/logs.d.ts
+++ src/types/logs.d.ts
@@ -1,7 +1,7 @@
 export interface Log {
   id: number;
 
-  type: 'add_account'| 'update_account' | 'remove_account' | 'transaction_income' | 'transaction_expense' | 'add_category';
+  type: 'add_account'| 'update_account' | 'remove_account' | 'transaction_income' | 'transaction_expense' | 'add_category' | 'update_category' | 'delete_category';
 
   description: string;
 
